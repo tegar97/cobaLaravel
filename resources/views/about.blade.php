0f02883ac07e9d477cb0e4fb82d5ea9c531fb8c4
tegar97 <eategar@gmail.com>
2020-01-13 19:55:20 +0700

    menambahkan file cobaLaravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/template')
+
+@section('title' ,'about')
+
+@section('container')
+   <div class="container">
+       <div class="row">
+           <div class="col-10"> 
+           <h1 class="mt-3">Hello ,{{$nama}}</h1>
+        </div>
+   </div> 
+</div>
+
+@endsection
